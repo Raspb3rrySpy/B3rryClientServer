77120c7baf05ee5bc69adade0c1ce55b501ff471
TheTridentGuy <99673331+TheTridentGuy@users.noreply.github.com>
2022-12-04 21:22:20 -0800

    create all files

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # B3rryClientServer
 
 Codebase for Raspb3rry Spy Project's B3rry Client, the control client for the B3rry Rover Robot.
-Version 1.0 Pre-release
+
+> # License:
+> GNU Gpl V3: See <https://www.gnu.org/licenses/>
+> 
+> ![](https://www.gnu.org/graphics/gplv3-with-text-136x68.png)
+
+
--- server.py
+++ server.py
@@ -1,8 +1,26 @@
+"""
+server.py
+Flask server for B3rry client
+Copyright (C) 2022  Aiden Bohlander
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 # Import necessary libraries
 import base64
 
 import cv2
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -10,21 +28,26 @@ app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
 
-@app.route("/frame")
-def get_frame():
-    success, frame = camera.read()  # read the camera frame
-    if success:
-        # encode image as jpeg
-        _, img_encoded = cv2.imencode('.jpg', frame)
-        base64_utf8_str = base64.b64encode(img_encoded).decode('utf-8')
-        return f'<img onload="setTimeout(function(){{this.reload()}}, 100)" src="data:image/jpeg;base64,' \
-               f'{base64_utf8_str}">'
+def get_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route("/index")
+@app.route('/stream')
+def stream():
+    return Response(get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
 def index():
-    with open("index.html") as indexfile:
-        return render_template(indexfile.read())
+    return render_template("index.html")
 
 
 app.run(port=5000)
